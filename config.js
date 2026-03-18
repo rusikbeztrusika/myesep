429260d8d5c321ba26fa3aebec90802e7d8584ae
rusikbeztrusika <rusikbeztrusika@gmail.com>
2026-03-18 16:13:29 +0500

    Add files via upload

--- config.js
+++ config.js
@@ -2,6 +2,7 @@
   supabaseUrl: "https://qayvdwitfdqptzgguoyj.supabase.co",
   supabaseAnonKey: "sb_publishable_GTAIrxKZyx5wXQYSS-NbqA_5a5DhfZB",
   feedbackWebhookUrl: "",
+  telegramBotToken: "8664044165:AAGG8yvNR-DA7QrZJAmJuiYzspejTeFvwfo",
   appName: "MyEsep",
   locale: "ru-KZ",
   currency: "KZT",
